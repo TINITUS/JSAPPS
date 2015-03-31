7875f11d620e9de5553be4a7cd130303e9c1bb75
TINITUS <mzografski@gmail.com>
2015-03-31 10:21:46 +0300

    Some refinements

--- HWJSAPPS5/books.js
+++ HWJSAPPS5/books.js
@@ -1,31 +1,32 @@
-(function(){
+(function () {
+	'use strict';
 	var RQST_URL = 'https://api.parse.com/1/classes/Books',
         $main = $('div#main');
 	
-	function loadBooks (data) {	
+	function loadBooks(data) {
 		$('.initLoader').show();
 		$main.html('');
 		var last = data.results.length - 1;
-		$.each(data.results, function (index, value) {			
+		$.each(data.results, function (index, value) {
 			var bookTemplate   = '<div class="media jumbotron" id="' + value.objectId + '">' +
-            							'<a class="media pull-left" traget="_blank">' +
-								    		'<img alt="' + value.bookTitle + '">' +								    		
-								  		'</a>' +
-								  		'<div class="media-body">' +
-								    		'<h2 class="media-heading">' + value.bookTitle + '</h2>' +
-								    		'<h4>' + value.bookAuthor +
-								    		'</br><span class="label label-info">ISBN: ' + value.bookISBN + '</span></h4>' +
-								    		'<p class="bookDesc">' + 				                	        	
-					                    	'</p>' +
-								  		'</div>' +
-								  		'<div class="pull-right">' + 
-					                    		'<button data-book="' + value.objectId + '" data-toggle="modal" data-target="#editAddModal" class="btn btn-warning editBook"><span class="glyphicon glyphicon-edit"></span> Edit</a>' +
-					                    		'<button data-book="' + value.objectId + '" data-toggle="modal" data-target="#deleteModal" class="btn btn-danger deleteBook"><span class="glyphicon glyphicon-remove"></span> Delete</a>' +
-					                    '</div>' +
+										'<a class="media pull-left" traget="_blank">' +
+											'<img alt="' + value.bookTitle + '">' +
+										'</a>' +
+										'<div class="media-body">' +
+											'<h2 class="media-heading">' + value.bookTitle + '</h2>' +
+											'<h4>' + value.bookAuthor +
+											'</br><span class="label label-info">ISBN: ' + value.bookISBN + '</span></h4>' +
+											'<p class="bookDesc">' +
+											'</p>' +
+										'</div>' +
+										'<div class="pull-right">' +
+												'<button data-book="' + value.objectId + '" data-toggle="modal" data-target="#editAddModal" class="btn btn-warning editBook"><span class="glyphicon glyphicon-edit"></span> Edit</a>' +
+												'<button data-book="' + value.objectId + '" data-toggle="modal" data-target="#deleteModal" class="btn btn-danger deleteBook"><span class="glyphicon glyphicon-remove"></span> Delete</a>' +
+										'</div>' +
 									'</div>';
 			$main.append($(bookTemplate));
 			getBookTumb(value.bookISBN, value.objectId);
-			if(index == last){
+			if (index === last) {
 				$('.initLoader').hide();
 			}
 		});
@@ -35,15 +36,15 @@
 		var $button = $(event.relatedTarget),
 			bookId  = $button.data('book'),
 			$modal  = $(this),
-			title   = $button.parents('#'+bookId).find('.media-heading').text();
+			title   = $button.parents('#' + bookId).find('.media-heading').text();
 
 		$modal.find('.modal-title').text('Delete Book ' + title);
-		$modal.find('.modal-body .submitBtn').on('click', {
-						'id':bookId,
-						'modal': $modal
-					},
-					deleteBook
-		);
+		$modal.find('.modal-body .submitBtn').on('click',
+			{
+				'id' : bookId,
+				'modal': $modal
+			},
+			deleteBook);
 	});
 
 	$('#editAddModal').on('show.bs.modal', function (event) {
@@ -51,27 +52,27 @@
 			bookId = $button.data('book'),
 			$modal = $(this);
 
-		if($button.hasClass('editBook')){
+		if ($button.hasClass('editBook')) {
 			//Edit Modal
-			var promise = ajaxRequester.get(RQST_URL + '/' + bookId);			
-				promise.success(function(data){			
+			var promise = ajaxRequester.get(RQST_URL + '/' + bookId);
+			promise.success(function (data) {
 			    var author = data.bookAuthor,
-			    	title = data.bookTitle, 
-			    	isbn = data.bookISBN;
+					title = data.bookTitle, 
+					isbn = data.bookISBN;
 
 				$modal.find('.modal-title').text('Edit Book ' + title);
 
-		  		$modal.find('.modal-body input#title').val(title);
-		  		$modal.find('.modal-body input#author').val(author);
-		  		$modal.find('.modal-body input#isbn').val(isbn);
-		  		$modal.find('.modal-footer .submitBtn').text('Update').on('click',{
+				$modal.find('.modal-body input#title').val(title);
+				$modal.find('.modal-body input#author').val(author);
+				$modal.find('.modal-body input#isbn').val(isbn);
+				$modal.find('.modal-footer .submitBtn').text('Update').on('click',{
 					'id':bookId,
 					'modal': $modal
 					},
 					editBook
 				);
 			});
-			promise.error(function(error){
+			promise.error(function (error) {
 				errorHandler(error);
 			});
 		} else if($button.hasClass('addBook')){
@@ -130,7 +131,7 @@
 
     	$('body').append('<div class="alert alert-danger alert-dismissible fade in" role="alert"><button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button><h3>' + message + '</h3></div>');
     		setTimeout(function () {
-				$('body').find('.alert').fadeOut('fast', function() {
+				$('body').find('.alert').fadeOut('fast', function () {
 					$(this).remove();
 				});
     		}, 5000);
@@ -139,7 +140,7 @@
 	function successMsg (message) {
     	$('body').append('<div class="alert alert-success alert-dismissible fade in" role="alert"><button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button><h3>' + message + '</h3></div>');
     		setTimeout(function () {
-				$('body').find('.alert').fadeOut('fast', function() {
+				$('body').find('.alert').fadeOut('fast', function () {
 					$(this).remove();
 				});
     		}, 5000);
@@ -155,7 +156,7 @@
 			url: 'https://www.googleapis.com/books/v1/volumes',
 			contentType: 'application/json',
 			data:{'q':((isbn && (isbn != '')) ? 'isbn:'+isbn : ' ')},
-			success: function(data){
+			success: function (data) {
 				if(data.items.length > 0){
 					var $item = $main.find('#'+id);
 					$item.find('a').attr('href', data.items[0].accessInfo.webReaderLink);
@@ -168,7 +169,7 @@
 					$item.find('.bookDesc').html('No description is avalilable for this volume!');
 				}
 			},
-			error: function(err){
+			error: function (err) {
 				var $item = $main.find('#'+id);
 				$item.find('a').attr('href', '#');
 				$item.find('img').attr('src','http://placehold.it/128x168');
